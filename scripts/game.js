54ba1119cde4f29b75010b4e03bfd391cd471819
Hadi Darwish <hadi.darwish.03@gmail.com>
2022-09-16 14:44:34 +0300

    adding a function to randomize pics in cards

--- scripts/game.js
+++ scripts/game.js
@@ -5,4 +5,19 @@ const lives = document.querySelector(".lives");
 const livesCount = 5;
 
 //adding lives to page screen
-lives.innerHTML = livesCount;
+lives.textContent = livesCount;
+//all images of languages
+const images = [
+  "../images/C_Programming_Language.svg",
+  "../images/Python-logo-notext.svg",
+  "../images/Unofficial_JavaScript_logo.svg",
+  "../images/C_Programming_Language.svg",
+  "../images/Python-logo-notext.svg",
+  "../images/Unofficial_JavaScript_logo.svg",
+];
+//function to randomize images in cards
+const randomize = () => {
+  images.sort(() => Math.random() - 0.5);
+  console.log(images);
+};
+randomize();
